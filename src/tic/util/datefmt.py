92ed6a0059f79596bda18234fcecb53e2b072276
Sam Elkhateeb <same@nanosn.com>
2010-06-19 17:49:08 -0300

    WIP getting the components to talk with each other

---
+++ src/tic/util/datefmt.py
@@ -0,0 +1,396 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2003-2009 Edgewall Software
+# Copyright (C) 2003-2006 Jonas Borgström <jonas@edgewall.com>
+# Copyright (C) 2006 Matthew Good <trac@matt-good.net>
+# Copyright (C) 2005-2006 Christian Boos <cboos@neuf.fr>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution. The terms
+# are also available at http://trac.edgewall.org/wiki/TracLicense.
+#
+# This software consists of voluntary contributions made by many
+# individuals. For the exact contribution history, see the revision
+# history and logs, available at http://trac.edgewall.org/log/.
+#
+# Author: Jonas Borgström <jonas@edgewall.com>
+#         Matthew Good <trac@matt-good.net>
+
+import sys
+import time
+
+from datetime import date
+from datetime import datetime
+from datetime import timedelta
+from datetime import tzinfo
+import locale
+import re
+from tic.core import TracError
+
+# Date/time utilities
+
+# -- conversion
+
+def to_datetime(t, tzinfo=None):
+    """Convert `t` into a `datetime` object, using the following rules:
+
+     - If `t` is already a `datetime` object, it is simply returned.
+     - If `t` is None, the current time will be used.
+     - If `t` is a number, it is interpreted as a timestamp.
+
+    If no `tzinfo` is given, the local timezone will be used.
+
+    Any other input will trigger a `TypeError`.
+    """
+    if t is None:
+        return datetime.now(tzinfo or localtz)
+    elif isinstance(t, datetime):
+        return t
+    elif isinstance(t, date):
+        return (tzinfo or localtz).localize(datetime(t.year, t.month, t.day))
+    elif isinstance(t, (int,long,float)):
+        return datetime.fromtimestamp(t, tzinfo or localtz)
+    raise TypeError('expecting datetime, int, long, float, or None; got %s' %
+                    type(t))
+
+def to_timestamp(dt):
+    """Return the corresponding POSIX timestamp"""
+    if dt:
+        diff = dt - _epoc
+        return diff.days * 86400 + diff.seconds
+    else:
+        return 0
+
+
+# -- formatting
+
+def pretty_timedelta(time1, time2=None, resolution=None):
+    """Calculate time delta between two `datetime` objects.
+    (the result is somewhat imprecise, only use for prettyprinting).
+
+    If either `time1` or `time2` is None, the current time will be used
+    instead.
+    """
+    time1 = to_datetime(time1)
+    time2 = to_datetime(time2)
+    if time1 > time2:
+        time2, time1 = time1, time2
+    units = ((3600 * 24 * 365, 'year',   'years'),
+             (3600 * 24 * 30,  'month',  'months'),
+             (3600 * 24 * 7,   'week',   'weeks'),
+             (3600 * 24,       'day',    'days'),
+             (3600,            'hour',   'hours'),
+             (60,              'minute', 'minutes'))
+    diff = time2 - time1
+    age_s = int(diff.days * 86400 + diff.seconds)
+    if resolution and age_s < resolution:
+        return ''
+    if age_s <= 60 * 1.9:
+        return '%i second%s' % (age_s, age_s != 1 and 's' or '')
+    for u, unit, unit_plural in units:
+        r = float(age_s) / float(u)
+        if r >= 1.9:
+            r = int(round(r))
+            return '%d %s' % (r, r == 1 and unit or unit_plural)
+    return ''
+
+def format_datetime(t=None, format='%x %X', tzinfo=None):
+    """Format the `datetime` object `t` into an `unicode` string
+
+    If `t` is None, the current time will be used.
+
+    The formatting will be done using the given `format`, which consist
+    of conventional `strftime` keys. In addition the format can be 'iso8601'
+    to specify the international date format.
+
+    `tzinfo` will default to the local timezone if left to `None`.
+    """
+    tz = tzinfo or localtz
+    t = to_datetime(t, tzinfo).astimezone(tz)
+    normalize_Z = False
+    if format.lower().startswith('iso8601'):
+        date_only = time_only = False
+        if 'date' in format:
+            date_only = True
+        elif 'time' in format:
+            time_only = True
+        if date_only:
+            format = '%Y-%m-%d'
+        elif time_only:
+            format = '%H:%M:%S'
+        else:
+            format = '%Y-%m-%dT%H:%M:%S'
+        if not date_only:
+            format += '%z'
+            normalize_Z = True
+    text = t.strftime(format)
+    if normalize_Z:
+        text = text.replace('+0000', 'Z')
+    encoding = locale.getpreferredencoding() or sys.getdefaultencoding()
+    if sys.platform != 'win32' or sys.version_info[:2] > (2, 3):
+        encoding = locale.getlocale(locale.LC_TIME)[1] or encoding
+        # Python 2.3 on windows doesn't know about 'XYZ' alias for 'cpXYZ'
+    return unicode(text, encoding, 'replace')
+
+def format_date(t=None, format='%x', tzinfo=None):
+    """Convenience method for formatting the date part of a `datetime` object.
+    See `format_datetime` for more details.
+    """
+    if format == 'iso8601':
+        format = 'iso8601date'
+    return format_datetime(t, format, tzinfo=tzinfo)
+
+def format_time(t=None, format='%X', tzinfo=None):
+    """Convenience method for formatting the time part of a `datetime` object.
+    See `format_datetime` for more details.
+    """
+    if format == 'iso8601':
+        format = 'iso8601time'
+    return format_datetime(t, format, tzinfo=tzinfo)
+
+def get_date_format_hint():
+    """Present the default format used by `format_date` in a human readable
+    form.
+    This is a format that will be recognized by `parse_date` when reading a
+    date.
+    """
+    t = datetime(1999, 10, 29, tzinfo=utc)
+    tmpl = format_date(t, tzinfo=utc)
+    return tmpl.replace('1999', 'YYYY', 1).replace('99', 'YY', 1) \
+               .replace('10', 'MM', 1).replace('29', 'DD', 1)
+
+def get_datetime_format_hint():
+    """Present the default format used by `format_datetime` in a human readable
+    form.
+    This is a format that will be recognized by `parse_date` when reading a
+    date.
+    """
+    t = datetime(1999, 10, 29, 23, 59, 58, tzinfo=utc)
+    tmpl = format_datetime(t, tzinfo=utc)
+    return tmpl.replace('1999', 'YYYY', 1).replace('99', 'YY', 1) \
+               .replace('10', 'MM', 1).replace('29', 'DD', 1) \
+               .replace('23', 'hh', 1).replace('11', 'hh', 1) \
+               .replace('59', 'mm', 1).replace('58', 'ss', 1)
+
+def http_date(t=None):
+    """Format `datetime` object `t` as a rfc822 timestamp"""
+    t = to_datetime(t).astimezone(utc)
+    weekdays = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
+    months = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',
+              'Oct', 'Nov', 'Dec')
+    return '%s, %02d %s %04d %02d:%02d:%02d GMT' % (
+        weekdays[t.weekday()], t.day, months[t.month - 1], t.year,
+        t.hour, t.minute, t.second)
+
+
+# -- parsing
+
+_ISO_8601_RE = re.compile(r'(\d\d\d\d)(?:-?(\d\d)(?:-?(\d\d))?)?'   # date
+                          r'(?:T(\d\d)(?::?(\d\d)(?::?(\d\d))?)?)?' # time
+                          r'(Z?(?:([-+])?(\d\d):?(\d\d)?)?)?$'      # timezone
+                          )
+
+def parse_date(text, tzinfo=None):
+    tzinfo = tzinfo or localtz
+    if text == 'now': # TODO: today, yesterday, etc.
+        return datetime.now(utc)
+    tm = None
+    text = text.strip()
+    # normalize ISO time
+    match = _ISO_8601_RE.match(text)
+    if match:
+        try:
+            g = match.groups()
+            years = g[0]
+            months = g[1] or '01'
+            days = g[2] or '01'
+            hours, minutes, seconds = [x or '00' for x in g[3:6]]
+            z, tzsign, tzhours, tzminutes = g[6:10]
+            if z:
+                tz = timedelta(hours=int(tzhours or '0'),
+                               minutes=int(tzminutes or '0')).seconds / 60
+                if tz == 0:
+                    tzinfo = utc
+                else:
+                    tzinfo = FixedOffset(tzsign == '-' and -tz or tz,
+                                         '%s%s:%s' %
+                                         (tzsign, tzhours, tzminutes))
+            tm = time.strptime('%s ' * 6 % (years, months, days,
+                                            hours, minutes, seconds),
+                               '%Y %m %d %H %M %S ')
+        except ValueError:
+            pass
+    else:
+        for format in ['%x %X', '%x, %X', '%X %x', '%X, %x', '%x', '%c',
+                       '%b %d, %Y']:
+            try:
+                tm = time.strptime(text, format)
+                break
+            except ValueError:
+                continue
+    if tm == None:
+        hint = get_date_format_hint()
+        raise TracError('"%s" is an invalid date, or the date format '
+                        'is not known. Try "%s" instead.' % (text, hint),
+                        'Invalid Date')
+    dt = tzinfo.localize(datetime(*tm[0:6]))
+    # Make sure we can convert it to a timestamp and back - fromtimestamp()
+    # may raise ValueError if larger than platform C localtime() or gmtime()
+    try:
+        to_datetime(to_timestamp(dt), tzinfo)
+    except ValueError:
+        raise TracError('The date "%s" is outside valid range. '
+                        'Try a date closer to present time.' % (text,),
+                        'Invalid Date')
+    return dt
+
+
+# -- timezone utilities
+
+class FixedOffset(tzinfo):
+    """Fixed offset in minutes east from UTC."""
+
+    def __init__(self, offset, name):
+        self._offset = timedelta(minutes=offset)
+        self.zone = name
+
+    def __str__(self):
+        return self.zone
+
+    def __repr__(self):
+        return '<FixedOffset "%s" %s>' % (self.zone, self._offset)
+
+    def utcoffset(self, dt):
+        return self._offset
+
+    def tzname(self, dt):
+        return self.zone
+
+    def dst(self, dt):
+        return _zero
+
+    def localize(self, dt, is_dst=False):
+        if dt.tzinfo is not None:
+            raise ValueError('Not naive datetime (tzinfo is already set)')
+        return dt.replace(tzinfo=self)
+
+
+STDOFFSET = timedelta(seconds=-time.timezone)
+if time.daylight:
+    DSTOFFSET = timedelta(seconds=-time.altzone)
+else:
+    DSTOFFSET = STDOFFSET
+
+DSTDIFF = DSTOFFSET - STDOFFSET
+
+
+class LocalTimezone(tzinfo):
+    """A 'local' time zone implementation"""
+
+    def utcoffset(self, dt):
+        if self._isdst(dt):
+            return DSTOFFSET
+        else:
+            return STDOFFSET
+
+    def dst(self, dt):
+        if self._isdst(dt):
+            return DSTDIFF
+        else:
+            return _zero
+
+    def tzname(self, dt):
+        return time.tzname[self._isdst(dt)]
+
+    def _isdst(self, dt):
+        tt = (dt.year, dt.month, dt.day,
+              dt.hour, dt.minute, dt.second,
+              dt.weekday(), 0, -1)
+        try:
+            stamp = time.mktime(tt)
+            tt = time.localtime(stamp)
+            return tt.tm_isdst > 0
+        except OverflowError:
+            return False
+
+    def localize(self, dt, is_dst=False):
+        if dt.tzinfo is not None:
+            raise ValueError('Not naive datetime (tzinfo is already set)')
+        return dt.replace(tzinfo=self)
+
+
+utc = FixedOffset(0, 'UTC')
+utcmin = datetime.min.replace(tzinfo=utc)
+utcmax = datetime.max.replace(tzinfo=utc)
+_epoc = datetime(1970, 1, 1, tzinfo=utc)
+_zero = timedelta(0)
+
+localtz = LocalTimezone()
+
+# Use a makeshift timezone implementation if pytz is not available.
+# This implementation only supports fixed offset time zones.
+#
+_timezones = [
+    FixedOffset(0, 'UTC'),
+    FixedOffset(-720, 'GMT -12:00'), FixedOffset(-660, 'GMT -11:00'),
+    FixedOffset(-600, 'GMT -10:00'), FixedOffset(-540, 'GMT -9:00'),
+    FixedOffset(-480, 'GMT -8:00'),  FixedOffset(-420, 'GMT -7:00'),
+    FixedOffset(-360, 'GMT -6:00'),  FixedOffset(-300, 'GMT -5:00'),
+    FixedOffset(-240, 'GMT -4:00'),  FixedOffset(-180, 'GMT -3:00'),
+    FixedOffset(-120, 'GMT -2:00'),  FixedOffset(-60, 'GMT -1:00'),
+    FixedOffset(0, 'GMT'),           FixedOffset(60, 'GMT +1:00'),
+    FixedOffset(120, 'GMT +2:00'),   FixedOffset(180, 'GMT +3:00'),
+    FixedOffset(240, 'GMT +4:00'),   FixedOffset(300, 'GMT +5:00'),
+    FixedOffset(360, 'GMT +6:00'),   FixedOffset(420, 'GMT +7:00'),
+    FixedOffset(480, 'GMT +8:00'),   FixedOffset(540, 'GMT +9:00'),
+    FixedOffset(600, 'GMT +10:00'),  FixedOffset(660, 'GMT +11:00'),
+    FixedOffset(720, 'GMT +12:00'),  FixedOffset(780, 'GMT +13:00')]
+_tzmap = dict([(z.zone, z) for z in _timezones])
+
+all_timezones = [z.zone for z in _timezones]
+
+try:
+    import pytz
+
+    _tzoffsetmap = dict([(tz.utcoffset(None), tz) for tz in _timezones
+                         if tz.zone != 'UTC'])
+
+    def timezone(tzname):
+        tz = get_timezone(tzname)
+        if not tz:
+            raise KeyError(tzname)
+        return tz
+
+    def get_timezone(tzname):
+        """Fetch timezone instance by name or return `None`"""
+        try:
+            # if given unicode parameter, pytz.timezone fails with:
+            # "type() argument 1 must be string, not unicode"
+            tz = pytz.timezone(unicode(tzname).encode('ascii', 'replace'))
+        except (KeyError, IOError):
+            tz = _tzmap.get(tzname)
+        if tz and tzname.startswith('Etc/'):
+            tz = _tzoffsetmap.get(tz.utcoffset(None))
+        return tz
+
+    _pytz_zones = [tzname for tzname in pytz.common_timezones
+                   if not tzname.startswith('Etc/') and
+                      not tzname.startswith('GMT')]
+    # insert just the GMT timezones into the pytz zones at the right location
+    # the pytz zones already include UTC so skip it
+    from bisect import bisect
+    _gmt_index = bisect(_pytz_zones, 'GMT')
+    all_timezones = _pytz_zones[:_gmt_index] + all_timezones[1:] + \
+                    _pytz_zones[_gmt_index:]
+
+except ImportError:
+
+    def timezone(tzname):
+        """Fetch timezone instance by name or raise `KeyError`"""
+        return _tzmap[tzname]
+
+    def get_timezone(tzname):
+        """Fetch timezone instance by name or return `None`"""
+        return _tzmap.get(tzname)
+
--- src/tic/web/main.py
+++ src/tic/web/main.py
@@ -15,13 +15,18 @@
 # limitations under the License.
 #
 import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 from tic.core import Component
-from tic.core import ComponentManager, ComponentMeta
+from tic.core import ComponentManager
 from tic.core import ExtensionPoint
-from tic.core import Interface
 from tic.core import implements
-from django.template import Template, Context
+from tic.web.api import IAuthenticator
+from tic.web.api import IRequestFilter
+from tic.web.api import IRequestHandler
+from tic.web.api import Request
+from tic.web.api import RequestDone
+
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 def dispatch_request(environ, start_response):
     """Main entry point for the Trac web interface.
@@ -31,40 +36,192 @@ def dispatch_request(environ, start_response):
     """
 
     env = Enviornment()
-    
-    todo_list = TodoList(env)
+    req = Request(environ, start_response)
 
-    todo_list.add('Make coffee',
-                  'Really need to make some coffee')
-    todo_list.add('Bug triage',
-                  'Double-check that all known issues were addressed')
-
-    t = Template("My name is : {{ my_name }}.")
-    c = Context({"my_name": "Adrian"})
-    print t.render(c)
+    try:
+        dispatcher = RequestDispatcher(env)
+        dispatcher.dispatch(req)
+    except RequestDone:
+        pass
+    resp = req._response or []
+    return resp
 
 class Enviornment(Component, ComponentManager):
     pass
 
-class ITodoObserver(Interface):
-    def todo_added(name, description):
-        """Called when a to-do item is added."""
+class RequestDispatcher(Component):
+    """Web request dispatcher.
 
-class TodoList(Component):
-    observers = ExtensionPoint(ITodoObserver)
+    This component dispatches incoming requests to registered handlers.
+    """
+    required = True
 
-    def __init__(self):
-        self.todos = {}
+    authenticators = ExtensionPoint(IAuthenticator)
+    handlers = ExtensionPoint(IRequestHandler)
 
-    def add(self, name, description):
-        assert not name in self.todos, 'To-do already in list'
-        self.todos[name] = description
-        for observer in self.observers:
-            observer.todo_added(name, description)
+    filters = ExtensionPoint(IRequestFilter)
+    
+#    filters = OrderedExtensionsOption('trac', 'request_filters',
+#                                      IRequestFilter,
+#                                      doc="""Ordered list of filters to apply to all requests
+#            (''since 0.10'').""")
+#
+#    default_handler = MyBestHandler()
+#    default_handler = ExtensionOption('trac', 'default_handler',
+#                                      IRequestHandler, 'WikiModule',
+#                                      """Name of the component that handles requests to the base URL.
+#
+#        Options include `TimelineModule`, `RoadmapModule`, `BrowserModule`,
+#        `QueryModule`, `ReportModule`, `TicketModule` and `WikiModule`. The
+#        default is `WikiModule`. (''since 0.9'')""")
+#
+#    default_timezone = Option('trac', 'default_timezone', '',
+#                              """The default timezone to use""")
+#
+#    # Public API
+#
+#    def authenticate(self, req):
+#        for authenticator in self.authenticators:
+#            authname = authenticator.authenticate(req)
+#            if authname:
+#                return authname
+#        else:
+#            return 'anonymous'
 
-class TodoPrinter(Component):
-    implements(ITodoObserver)
+    def dispatch(self, req):
+        print "now we are starting"
+        a = MyBestHandler(self.compmgr)
+        a.process_request(req);
+#        self.default_handler.process_request()
+#        """Find a registered handler that matches the request and let it process
+#        it.
+#
+#        In addition, this method initializes the HDF data set and adds the web
+#        site chrome.
+#        """
+#        self.log.debug('Dispatching %r', req)
+#        chrome = Chrome(self.env)
+#
+#        # Setup request callbacks for lazily-evaluated properties
+#        req.callbacks.update({
+#                             'authname': self.authenticate,
+#                             'chrome': chrome.prepare_request,
+#                             'hdf': self._get_hdf,
+#                             'perm': self._get_perm,
+#                             'session': self._get_session,
+#                             'locale': self._get_locale,
+#                             'tz': self._get_timezone,
+#                             'form_token': self._get_form_token
+#                             })
+#
+#        try:
+#            try:
+#                # Select the component that should handle the request
+#                chosen_handler = None
+#                try:
+#                    for handler in self.handlers:
+#                        if handler.match_request(req):
+#                            chosen_handler = handler
+#                            break
+#                    if not chosen_handler:
+#                        if not req.path_info or req.path_info == '/':
+#                            chosen_handler = self.default_handler
+#                    # pre-process any incoming request, whether a handler
+#                    # was found or not
+#                    chosen_handler = self._pre_process_request(req,
+#                                                               chosen_handler)
+#                except TracError, e:
+#                    raise HTTPInternalError(e)
+#                if not chosen_handler:
+#                    if req.path_info.endswith('/'):
+#                        # Strip trailing / and redirect
+#                        target = req.path_info.rstrip('/').encode('utf-8')
+#                        if req.query_string:
+#                            target += '?' + req.query_string
+#                        req.redirect(req.href + target, permanent=True)
+#                    raise HTTPNotFound('No handler matched request to %s',
+#                                       req.path_info)
+#
+#                req.callbacks['chrome'] = partial(chrome.prepare_request,
+#                                                  handler=chosen_handler)
+#
+#                # Protect against CSRF attacks: we validate the form token
+#                # for all POST requests with a content-type corresponding
+#                # to form submissions
+#                if req.method == 'POST':
+#                    ctype = req.get_header('Content-Type')
+#                    if ctype:
+#                        ctype, options = cgi.parse_header(ctype)
+#                    if ctype in ('application/x-www-form-urlencoded',
+#                                 'multipart/form-data') and \
+#                        req.args.get('__FORM_TOKEN') != req.form_token:
+#                    if self.env.secure_cookies and req.scheme == 'http':
+#                        msg = _('Secure cookies are enabled, you must '
+#                                'use https to submit forms.')
+#                    else:
+#                        msg = _('Do you have cookies enabled?')
+#                    raise HTTPBadRequest(_('Missing or invalid form token.'
+#                                         ' %(msg)s', msg=msg))
+#
+#                # Process the request and render the template
+#                resp = chosen_handler.process_request(req)
+#                if resp:
+#                    if len(resp) == 2: # Clearsilver
+#                        chrome.populate_hdf(req)
+#                        template, content_type = \
+#                            self._post_process_request(req, * resp)
+#                        # Give the session a chance to persist changes
+#                        req.session.save()
+#                        req.display(template, content_type or 'text/html')
+#                    else: # Genshi
+#                        template, data, content_type = \
+#                            self._post_process_request(req, * resp)
+#                        if 'hdfdump' in req.args:
+#                            req.perm.require('TRAC_ADMIN')
+#                            # debugging helper - no need to render first
+#                            out = StringIO()
+#                            pprint(data, out)
+#                            req.send(out.getvalue(), 'text/plain')
+#                        else:
+#                            output = chrome.render_template(req, template,
+#                                                            data,
+#                                                            content_type)
+#                            # Give the session a chance to persist changes
+#                            req.session.save()
+#                            req.send(output, content_type or 'text/html')
+#                else:
+#                    self._post_process_request(req)
+#            except RequestDone:
+#                raise
+#            except:
+#                # post-process the request in case of errors
+#                err = sys.exc_info()
+#                try:
+#                    self._post_process_request(req)
+#                except RequestDone:
+#                    raise
+#                except Exception, e:
+#                    self.log.error("Exception caught while post-processing"
+#                                   " request: %s",
+#                                   exception_to_unicode(e, traceback=True))
+#                raise err[0], err[1], err[2]
+#        except PermissionError, e:
+#            raise HTTPForbidden(to_unicode(e))
+#        except ResourceNotFound, e:
+#            raise HTTPNotFound(e)
+#        except TracError, e:
+#            raise HTTPInternalError(e)
 
-    def todo_added(self, name, description):
-        print 'TODO', name
-        print '     ', description
+class MyBestHandler(Component):
+    implements(IRequestHandler)
+    
+    def match_request(self, req):
+        return True
+
+    def process_request(self, req):
+        print "yay"
+
+
+
+
+    

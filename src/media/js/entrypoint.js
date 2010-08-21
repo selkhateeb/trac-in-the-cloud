f306f268063549deb12505d4958474cca8405e72
Sam Elkhateeb <same@nanosn.com>
2010-08-21 18:17:26 -0300

    added main entry point for the application

---
+++ src/media/js/entrypoint.js
@@ -0,0 +1,9 @@
+
+dojo.addOnLoad(function(){
+    dojo.addOnLoad(function(){
+        div = dojo.create("div");
+        div.innerHTML = "This is a demo";
+        dojo.body().appendChild(div);
+    });
+});
+
